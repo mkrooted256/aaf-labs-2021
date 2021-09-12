18cecd0e89c5efa0a1c9e948c84df9a07330ca0c
mkrooted256 <koreshov.m@gmail.com>
2021-09-12 14:31:56 +0300

    fi91-koreshkov: initial commit (try my main.exe)

---
+++ FI91_Koreshkov/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+
+int main() {
+    cout << "Guten Tag, Welt!" << endl;
+    system("explorer \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"");
+}
