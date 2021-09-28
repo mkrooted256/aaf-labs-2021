2d3e0d77c7af8de8fa91b2c8760d4dff631c1f1c
mkrooted256 <koreshov.m@gmail.com>
2021-09-28 15:50:28 +0300

    Add: Set constructors

--- shmorhun_fi-91_koreshkov_fi-91/src/Set.h
+++ shmorhun_fi-91_koreshkov_fi-91/src/Set.h
@@ -9,6 +9,13 @@ public:
 	static const size_t DEFAULT_SZ = 8;
 	std::vector<int> data;
 
+	Set(std::vector<int> _data) {
+		std::sort(_data.begin(), _data.end());
+		std::unique(_data.begin(), _data.end());
+		data = _data;
+	}
+	Set() {}
+
 	bool Contains(int x);
 	bool Insert(int x);
 	bool Delete(int x);
